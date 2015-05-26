ef4df47793d08f50165263487291d2ffcf82f645
ChanMo <chen.orange@aliyun.com>
2015-05-26 22:03:13 +0800

    mkdir theme folder

--- page/views.py
+++ page/views.py
@@ -9,12 +9,20 @@ class IndexView(TemplateView):
 
 
 class SiteView(TemplateView):
-    template_name = "page/index.html"
+    modal = Site
     def get_context_data(self, **kwargs):
         context = super(SiteView, self).get_context_data(**kwargs)
         site = Site.objects.filter(author=context['site_id'])
+        self.site = site
         context['page_list'] = Page.objects.filter(site=site,parent=None)
         return context
+
+    def get_template_names(self):
+        print self.site.id
+        #theme = self.site.theme_id
+        theme = "default"
+
+        return ("page/"+theme+"/index.html", "page/default/index.html")
 
 
 class PageView(DetailView):
